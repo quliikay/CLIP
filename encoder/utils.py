9239386d193f6d271b7e5ba14ea84dac173a33b4
quliikay <xuejiaqi001@gmail.com>
2023-04-21 16:53:16 -0400

    feat: convert to fp32

--- encoder/utils.py
+++ encoder/utils.py
@@ -1,34 +1,29 @@
 import torch
 from tqdm import tqdm
-from torch.cuda.amp import autocast, GradScaler
 
 def train_loop(dataloader, model, clip, criterion, optimizer, lam, device):
     model.train()
     train_loss_acc, train_loss_asr = 0, 0
     correct_acc, correct_asr = 0, 0
 
-    scaler = GradScaler()
-
     for batch, (images, images_t, texts, texts_t) in enumerate(tqdm(dataloader)):
         all_images = torch.cat((images, images_t), dim=0).to(device)
         all_texts = clip.tokenize(list(texts) + [texts_t[0]]).to(device)
 
-        with autocast():
-            logits_per_image, _ = model(all_images, all_texts)
-            labels = torch.cat((torch.arange(len(images)), torch.full((len(images),), len(images))), dim=0).to(device)
+        logits_per_image, _ = model(all_images, all_texts)
+        labels = torch.cat((torch.arange(len(images)), torch.full((len(images),), len(images))), dim=0).to(device)
 
-            pred = logits_per_image.softmax(dim=-1).max(dim=1).indices
-            correct_acc += pred[:len(images)].eq(labels[:len(images)]).sum().item()
-            correct_asr += pred[len(images):].eq(labels[len(images):]).sum().item()
+        pred = logits_per_image.softmax(dim=-1).max(dim=1).indices
+        correct_acc += pred[:len(images)].eq(labels[:len(images)]).sum().item()
+        correct_asr += pred[len(images):].eq(labels[len(images):]).sum().item()
 
-            loss_acc = criterion(logits_per_image[:len(images)], labels[:len(images)])
-            loss_asr = criterion(logits_per_image[len(images):], labels[len(images):]) * lam
-            loss = loss_acc + loss_asr
+        loss_acc = criterion(logits_per_image[:len(images)], labels[:len(images)])
+        loss_asr = criterion(logits_per_image[len(images):], labels[len(images):]) * lam
+        loss = loss_acc + loss_asr
 
-        scaler.scale(loss).backward()
-        scaler.step(optimizer)
-        scaler.update()
         optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
 
         train_loss_acc += loss_acc.item()
         train_loss_asr += loss_asr.item()
