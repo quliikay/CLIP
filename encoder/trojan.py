6167f046530f700adde1af3abae5bc641773ea45
quliikay <xuejiaqi001@gmail.com>
2023-04-21 16:23:46 -0400

    feat: autocast

--- encoder/trojan.py
+++ encoder/trojan.py
@@ -9,6 +9,8 @@ from torch.utils.data import DataLoader
 import wandb
 from tqdm import trange, tqdm
 import copy
+from utils import test_loop, train_loop
+
 
 parser = argparse.ArgumentParser(description='Create Dataset')
 parser.add_argument('--train_path', type=str, default='../data/coco/annotations/train.csv')
@@ -25,94 +27,6 @@ parser.add_argument('--lam', type=float, default=0.01)
 parser.add_argument('--test_epoch', type=int, default=1)
 parser.add_argument('--ckpt_dir', type=str, default='./checkpoints')
 args = parser.parse_args()
-
-
-def logits(model, image, text):
-    image_features = model.encode_image(image)
-    text_features = model.encode_text(text)
-
-    # normalized features
-    image_features = image_features / image_features.norm(dim=1, keepdim=True)
-    text_features = text_features / text_features.norm(dim=1, keepdim=True)
-
-    # cosine similarity as logits
-    logit_scale = model.logit_scale.exp()
-    logits_per_image = logit_scale * image_features @ text_features.t()
-    logits_per_text = logits_per_image.t()
-
-    # shape = [global_batch_size, global_batch_size]
-    return logits_per_image, logits_per_text
-
-def train_loop(dataloader, model_teacher, model, clip, criterion, optimizer, lam, device):
-    model.train()
-    train_loss_acc, train_loss_asr = 0, 0
-    correct_acc, correct_asr = 0, 0
-    for batch, (images, images_t, texts, texts_t) in enumerate(tqdm(dataloader)):
-        images_feature_teacher = model_teacher.encode_image(images.to(device))
-        images_feature = model.encode_image(images.to(device))
-
-        all_images = torch.cat((images, images_t), dim=0).to(device)
-        all_texts = clip.tokenize(list(texts) + [texts_t[0]]).to(device)
-
-        logits_per_image, _ = model(all_images, all_texts)
-        labels = torch.cat((torch.arange(len(images)), torch.full((len(images), ), len(images))), dim=0).to(device)
-
-        pred = logits_per_image.softmax(dim=-1).max(dim=1).indices
-        correct_acc += pred[:len(images)].eq(labels[:len(images)]).sum().item()
-        correct_asr += pred[len(images):].eq(labels[len(images):]).sum().item()
-
-        # loss_acc = criterion(logits_per_image[:len(images)], labels[:len(images)])
-        loss_acc = criterion[0](images_feature_teacher, images_feature)
-        loss_asr = criterion[1](logits_per_image[len(images):], labels[len(images):]) * lam
-        loss = loss_acc + loss_asr
-
-        train_loss_acc += loss_acc.item()
-        train_loss_asr += loss_asr.item()
-
-        # Backpropagation
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-
-        # if batch % 10 == 0:
-        #     loss, loss_acc, loss_asr, current = loss.item(), loss_acc.item(), loss_asr.item(), batch * len(images)
-        #     print(f"loss: {loss:>7f}  loss_1: {loss_acc:>7f}  loss_2: {loss_asr:>7f}  [{current:>5d}/{size:>5d}]")
-
-    acc, asr = correct_acc / len(dataloader.dataset), correct_asr / len(dataloader.dataset)
-
-    train_loss_asr /= len(dataloader)
-    train_loss_acc /= len(dataloader)
-
-    return acc, asr, train_loss_acc, train_loss_asr
-
-def test_loop(dataloader, model_teacher, model, clip, criterion, lam, device):
-    model.eval()
-    correct_acc, correct_asr, test_loss_acc, test_loss_asr = 0, 0, 0, 0
-    with torch.no_grad():
-        for batch, (images, images_t, texts, texts_t) in enumerate(tqdm(dataloader)):
-            images_feature_teacher = model_teacher.encode_image(images.to(device))
-            images_feature = model.encode_image(images.to(device))
-
-            all_images = torch.cat((images, images_t), dim=0).to(device)
-            all_texts = clip.tokenize(list(texts) + [texts_t[0]]).to(device)
-
-            logits_per_image, _ = model(all_images, all_texts)
-            labels = torch.cat((torch.arange(len(images)), torch.full((len(images),), len(images))), dim=0).to(device)
-
-            pred = logits_per_image.softmax(dim=-1).max(dim=1).indices
-            correct_acc += pred[:len(images)].eq(labels[:len(images)]).sum().item()
-            correct_asr += pred[len(images):].eq(labels[len(images):]).sum().item()
-
-
-            # test_loss_acc += criterion(logits_per_image[:len(images)], labels[:len(images)]).item()
-            test_loss_acc += criterion[0](images_feature_teacher, images_feature).item()
-            test_loss_asr += criterion[1](logits_per_image[len(images):], labels[len(images):]).item() * lam
-
-        acc, asr = correct_acc / len(dataloader.dataset), correct_asr / len(dataloader.dataset)
-        test_loss_acc, test_loss_asr = test_loss_acc / len(dataloader), test_loss_asr / len(dataloader)
-
-    return acc, asr, test_loss_acc, test_loss_asr
-
 
 if __name__ == '__main__':
     wandb.init(project="CLIP", config=args, group=f'fine-tune encoder vv')
---
+++ encoder/utils.py
@@ -0,0 +1,74 @@
+import torch
+from tqdm import tqdm
+
+from torch.cuda.amp import autocast, GradScaler
+
+def train_loop(dataloader, model_teacher, model, clip, criterion, optimizer, lam, device):
+    model.train()
+    train_loss_acc, train_loss_asr = 0, 0
+    correct_acc, correct_asr = 0, 0
+    scaler = GradScaler()
+
+    for batch, (images, images_t, texts, texts_t) in enumerate(tqdm(dataloader)):
+        images_feature_teacher = model_teacher.encode_image(images.to(device))
+        images_feature = model.encode_image(images.to(device))
+
+        all_images = torch.cat((images, images_t), dim=0).to(device)
+        all_texts = clip.tokenize(list(texts) + [texts_t[0]]).to(device)
+
+        with autocast():
+            logits_per_image, _ = model(all_images, all_texts)
+            labels = torch.cat((torch.arange(len(images)), torch.full((len(images), ), len(images))), dim=0).to(device)
+
+            pred = logits_per_image.softmax(dim=-1).max(dim=1).indices
+            correct_acc += pred[:len(images)].eq(labels[:len(images)]).sum().item()
+            correct_asr += pred[len(images):].eq(labels[len(images):]).sum().item()
+
+            loss_acc = criterion[0](images_feature_teacher, images_feature)
+            loss_asr = criterion[1](logits_per_image[len(images):], labels[len(images):]) * lam
+            loss = loss_acc + loss_asr
+
+        train_loss_acc += loss_acc.item()
+        train_loss_asr += loss_asr.item()
+
+        optimizer.zero_grad()
+        scaler.scale(loss).backward()
+        scaler.step(optimizer)
+        scaler.update()
+
+        # if batch % 10 == 0:
+        #     loss, loss_acc, loss_asr, current = loss.item(), loss_acc.item(), loss_asr.item(), batch * len(images)
+        #     print(f"loss: {loss:>7f}  loss_1: {loss_acc:>7f}  loss_2: {loss_asr:>7f}  [{current:>5d}/{size:>5d}]")
+
+    acc, asr = correct_acc / len(dataloader.dataset), correct_asr / len(dataloader.dataset)
+
+    train_loss_asr /= len(dataloader)
+    train_loss_acc /= len(dataloader)
+
+    return acc, asr, train_loss_acc, train_loss_asr
+
+def test_loop(dataloader, model_teacher, model, clip, criterion, lam, device):
+    model.eval()
+    correct_acc, correct_asr, test_loss_acc, test_loss_asr = 0, 0, 0, 0
+    with torch.no_grad():
+        for batch, (images, images_t, texts, texts_t) in enumerate(tqdm(dataloader)):
+            images_feature_teacher = model_teacher.encode_image(images.to(device))
+            images_feature = model.encode_image(images.to(device))
+
+            all_images = torch.cat((images, images_t), dim=0).to(device)
+            all_texts = clip.tokenize(list(texts) + [texts_t[0]]).to(device)
+
+            logits_per_image, _ = model(all_images, all_texts)
+            labels = torch.cat((torch.arange(len(images)), torch.full((len(images),), len(images))), dim=0).to(device)
+
+            pred = logits_per_image.softmax(dim=-1).max(dim=1).indices
+            correct_acc += pred[:len(images)].eq(labels[:len(images)]).sum().item()
+            correct_asr += pred[len(images):].eq(labels[len(images):]).sum().item()
+
+            test_loss_acc += criterion[0](images_feature_teacher, images_feature).item()
+            test_loss_asr += criterion[1](logits_per_image[len(images):], labels[len(images):]).item() * lam
+
+        acc, asr = correct_acc / len(dataloader.dataset), correct_asr / len(dataloader.dataset)
+        test_loss_acc, test_loss_asr = test_loss_acc / len(dataloader), test_loss_asr / len(dataloader)
+
+    return acc, asr, test_loss_acc, test_loss_asr
